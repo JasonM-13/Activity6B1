239c0093bae826b21b3a34ebd85076152bcc734e
KatsFried <jason.mong@ontariotechu.net>
2025-02-13 20:07:03 -0500

    fixed error

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
     public static void main(String [] args){
         //Print 10 times
         int count = 0;
-        for(int i = 0; i <= 10; i++){
+        for(int i = 0; i < 10; i++){
             System.out.println("Hello world");
             count++;
         }

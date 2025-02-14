4cc6d2e666cf0f3195cd477217a9f5424004519b
KatsFried <jason.mong@ontariotechu.net>
2025-02-13 20:03:13 -0500

    Created java file

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String [] args){
+        //Print 10 times
+        int count = 0;
+        for(int i = 0; i <= 10; i++){
+            System.out.println("Hello world");
+            count++;
+        }
+        System.out.println(count);
+    }
+}
